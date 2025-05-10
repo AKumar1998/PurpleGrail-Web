d0eb17eba8730d62707958a02b62c1e290accf99
Ajay Kumar <ajaybrowning13@gmail.com>
2025-05-10 15:49:50 +0530

    php changed

--- contact_email_service.php
+++ contact_email_service.php
@@ -3,22 +3,26 @@ header("Content-Type: application/json");
 header("Access-Control-Allow-Origin:https://www.purplegrail.in ;Access-Control-Allow-Methods: POST, OPTIONS");
 
 
-
-
 $method = $_SERVER['REQUEST_METHOD'];
-$input = json_decode(file_get_contents('php://input'));
 
 
 if($method == 'POST') {
-      $name=$input->name;
-      $email=$input->email;
-      $comments=$input->comments;
+      $name = $_POST['name'] ?? '';
+      $email = $_POST['email'] ?? '';
+      $comments = $_POST['comments'] ?? '';
+      $subject = "Website Enquiry";
       $to='purplegrailservices@gmail.com';
       $headers= 'From: purplegrail@purplegrail.in' . "\r\n" .
       'Content-Type: text/plain';
       $message =" Name:     $name \n Email:    $email \n \n $comments";
 
-      mail($to,"Website Enquiry",$message,$headers);
-      echo json_encode(['message' => 'success']);
+      // mail($to,"Website Enquiry",$message,$headers);
+      // echo json_encode(['message' => 'success']);
+
+      if (mail($to, $subject, $message, $headers)) {
+            echo "success";
+        } else {
+            echo "Error sending email.";
+        }
 }
 ?>
